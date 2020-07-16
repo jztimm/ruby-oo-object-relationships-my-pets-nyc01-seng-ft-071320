67eac80d799c0ebf2903b5d2ce3da1ded0647426
“Mineliegoma” <garlandegoma@gmail.com>
2020-07-16 10:29:48 -0400

    cat and dog .MG

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,17 @@
-class Cat
+class Cat  #MG
+  attr_reader :name
+  attr_accessor :owner, :mood
   # code goes here
-end
+  @@all= []
+  def initialize(name, owner)
+    @name= name
+    @owner=owner
+    @mood= "nervous"
+    @@all<< self
+  end
+
+
+  def self.all
+    @@all 
+  end
+end 
