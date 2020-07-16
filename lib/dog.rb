7c5d88efaa4c5e255da862093bc9c4fa813e4482
jztimm <jztimm@gmail.com>
2020-07-16 12:03:22 -0400

    Associations. JZ

--- lib/dog.rb
+++ lib/dog.rb
@@ -15,6 +15,7 @@ class Dog
   def self.all
     @@all 
   end
+  
 
 
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -27,5 +27,49 @@ class Owner
     @@all.clear
   end
 
+  def add_cat(cat)
+    cat.owner = self
+  end
+ 
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+
+  def add_dog(dog)
+    dog.owner = self
+  end
+ 
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+
+  def buy_cat(name)
+    cat = Cat.new(name, @owner)
+    cat.owner = self
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name, @owner)
+    dog.owner = self
+  end
+
+  def walk_dogs
+    Dog.all.each {|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    Cat.all.each {|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    pets = self.dogs + self.cats
+
+    pets.each {|pet| pet.mood = "nervous"
+    pet.owner = nil}
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
   
 end
